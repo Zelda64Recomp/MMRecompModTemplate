39df6d3bd5bc2b95f26e84d4d73a826ea26cfe66
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2025-01-31 12:11:43 -0500

    Add example toml/code for config values and example code for building a UI

---
+++ include/recompui.h
@@ -0,0 +1,229 @@
+#ifndef __RECOMPUI_H__
+#define __RECOMPUI_H__
+
+#include "modding.h"
+
+typedef unsigned long RecompuiContext;
+typedef unsigned long RecompuiResource;
+
+typedef enum {
+    CURSOR_NONE,
+    CURSOR_POINTER
+} RecompuiCursor;
+
+typedef enum {
+    EVENT_TYPE_NONE,
+    EVENT_TYPE_CLICK,
+    EVENT_TYPE_FOCUS,
+    EVENT_TYPE_HOVER,
+    EVENT_TYPE_ENABLE,
+    EVENT_TYPE_DRAG,
+    EVENT_TYPE_TEXT,
+    EVENT_TYPE_COUNT
+} RecompuiEventType;
+
+typedef enum {
+    DRAG_PHASE_NONE,
+    DRAG_PHASE_START,
+    DRAG_PHASE_MOVE,
+    DRAG_PHASE_END
+} RecompuiDragPhase;
+
+typedef enum {
+    DISPLAY_BLOCK,
+    DISPLAY_FLEX
+} RecompuiDisplay;
+
+typedef enum {
+    POSITION_ABSOLUTE,
+    POSITION_RELATIVE
+} RecompuiPosition;
+
+typedef enum {
+    JUSTIFY_CONTENT_FLEX_START,
+    JUSTIFY_CONTENT_FLEX_END,
+    JUSTIFY_CONTENT_CENTER,
+    JUSTIFY_CONTENT_SPACE_BETWEEN,
+    JUSTIFY_CONTENT_SPACE_AROUND,
+    JUSTIFY_CONTENT_SPACE_EVENLY
+} RecompuiJustifyContent;
+
+typedef enum {
+    FLEX_DIRECTION_ROW,
+    FLEX_DIRECTION_COLUMN
+} RecompuiFlexDirection;
+
+typedef enum {
+    ALIGN_ITEMS_FLEX_START,
+    ALIGN_ITEMS_FLEX_END,
+    ALIGN_ITEMS_CENTER,
+    ALIGN_ITEMS_BASELINE,
+    ALIGN_ITEMS_STRETCH
+} RecompuiAlignItems;
+
+typedef enum {
+    OVERFLOW_VISIBLE,
+    OVERFLOW_HIDDEN,
+    OVERFLOW_AUTO,
+    OVERFLOW_SCROLL
+} RecompuiOverflow;
+
+typedef enum {
+    UNIT_FLOAT,
+    UNIT_DP,
+    UNIT_PERCENT
+} RecompuiUnit;
+
+typedef enum {
+    ANIMATION_TYPE_NONE,
+    ANIMATION_TYPE_SET,
+    ANIMATION_TYPE_TWEEN
+} RecompuiAnimationType;
+
+typedef enum {
+    FONT_STYLE_NORMAL,
+    FONT_STYLE_ITALIC
+} RecompuiFontStyle;
+
+typedef enum {
+    TEXT_ALIGN_LEFT,
+    TEXT_ALIGN_RIGHT,
+    TEXT_ALIGN_CENTER,
+    TEXT_ALIGN_JUSTIFY
+} RecompuiTextAlign;
+
+typedef enum {
+    DRAG_NONE,
+    DRAG_DRAG,
+    DRAG_DRAG_DROP,
+    DRAG_BLOCK,
+    DRAG_CLONE
+} RecompuiDrag;
+
+typedef enum {
+    TAB_INDEX_NONE,
+    TAB_INDEX_AUTO
+} RecompuiTabIndex;
+
+typedef enum {
+    BUTTONSTYLE_PRIMARY,
+    BUTTONSTYLE_SECONDARY,
+} RecompuiButtonStyle;
+
+typedef struct {
+    unsigned long type;
+    float duration;
+} RecompuiAnimation;
+
+typedef struct {
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+    unsigned char a;
+} RecompuiColor;
+
+#define RECOMPUI_NULL_CONTEXT (0xFFFFFFFFUL)
+#define RECOMPUI_NULL_RESOURCE (0xFFFFFFFFUL)
+
+// Contexts
+RECOMP_IMPORT("*", RecompuiContext recompui_create_context());
+RECOMP_IMPORT("*", void recompui_open_context(RecompuiContext context));
+RECOMP_IMPORT("*", void recompui_close_context(RecompuiContext context));
+RECOMP_IMPORT("*", RecompuiResource recompui_context_root(RecompuiContext context));
+RECOMP_IMPORT("*", void recompui_show_context(RecompuiContext context));
+RECOMP_IMPORT("*", void recompui_hide_context(RecompuiContext context));
+
+// Resources
+RECOMP_IMPORT("*", RecompuiContext recompui_create_style(RecompuiContext context));
+RECOMP_IMPORT("*", RecompuiContext recompui_create_element(RecompuiContext context, RecompuiResource parent));
+RECOMP_IMPORT("*", RecompuiContext recompui_create_button(RecompuiContext context, RecompuiResource parent, const char* text, RecompuiButtonStyle style));
+
+// Position and Layout
+RECOMP_IMPORT("*", void recompui_set_position(RecompuiResource id, RecompuiPosition position));
+RECOMP_IMPORT("*", void recompui_set_left(RecompuiResource id, float left, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_top(RecompuiResource id, float top, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_right(RecompuiResource id, float right, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_bottom(RecompuiResource id, float bottom, RecompuiUnit unit));
+
+// Sizing
+RECOMP_IMPORT("*", void recompui_set_width(RecompuiResource id, float width, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_width_auto(RecompuiResource id));
+RECOMP_IMPORT("*", void recompui_set_height(RecompuiResource id, float height, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_height_auto(RecompuiResource id));
+RECOMP_IMPORT("*", void recompui_set_min_width(RecompuiResource id, float width, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_min_height(RecompuiResource id, float height, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_max_width(RecompuiResource id, float width, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_max_height(RecompuiResource id, float height, RecompuiUnit unit));
+
+// Padding
+RECOMP_IMPORT("*", void recompui_set_padding(RecompuiResource id, float padding, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_padding_left(RecompuiResource id, float padding, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_padding_top(RecompuiResource id, float padding, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_padding_right(RecompuiResource id, float padding, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_padding_bottom(RecompuiResource id, float padding, RecompuiUnit unit));
+
+// Margins
+RECOMP_IMPORT("*", void recompui_set_margin(RecompuiResource id, float margin, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_margin_left(RecompuiResource id, float margin, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_margin_top(RecompuiResource id, float margin, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_margin_right(RecompuiResource id, float margin, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_margin_bottom(RecompuiResource id, float margin, RecompuiUnit unit));
+
+// Borders
+RECOMP_IMPORT("*", void recompui_set_border_width(RecompuiResource id, float width, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_border_left_width(RecompuiResource id, float width, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_border_top_width(RecompuiResource id, float width, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_border_right_width(RecompuiResource id, float width, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_border_bottom_width(RecompuiResource id, float width, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_border_radius(RecompuiResource id, float radius, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_border_top_left_radius(RecompuiResource id, float radius, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_border_top_right_radius(RecompuiResource id, float radius, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_border_bottom_left_radius(RecompuiResource id, float radius, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_border_bottom_right_radius(RecompuiResource id, float radius, RecompuiUnit unit));
+
+// Colors
+RECOMP_IMPORT("*", void recompui_set_background_color(RecompuiResource id, const RecompuiColor* color));
+RECOMP_IMPORT("*", void recompui_set_border_color(RecompuiResource id, const RecompuiColor* color));
+RECOMP_IMPORT("*", void recompui_set_border_left_color(RecompuiResource id, const RecompuiColor* color));
+RECOMP_IMPORT("*", void recompui_set_border_top_color(RecompuiResource id, const RecompuiColor* color));
+RECOMP_IMPORT("*", void recompui_set_border_right_color(RecompuiResource id, const RecompuiColor* color));
+RECOMP_IMPORT("*", void recompui_set_border_bottom_color(RecompuiResource id, const RecompuiColor* color));
+RECOMP_IMPORT("*", void recompui_set_color(RecompuiResource id, const RecompuiColor* color));
+
+// Cursor and Display
+RECOMP_IMPORT("*", void recompui_set_cursor(RecompuiResource id, RecompuiCursor cursor));
+RECOMP_IMPORT("*", void recompui_set_opacity(RecompuiResource id, float opacity));
+RECOMP_IMPORT("*", void recompui_set_display(RecompuiResource id, RecompuiDisplay display));
+
+// Flexbox
+RECOMP_IMPORT("*", void recompui_set_justify_content(RecompuiResource id, RecompuiJustifyContent justify_content));
+RECOMP_IMPORT("*", void recompui_set_flex_grow(RecompuiResource id, float grow));
+RECOMP_IMPORT("*", void recompui_set_flex_shrink(RecompuiResource id, float shrink));
+RECOMP_IMPORT("*", void recompui_set_flex_basis_auto(RecompuiResource id));
+RECOMP_IMPORT("*", void recompui_set_flex_basis(RecompuiResource id, float basis, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_flex_direction(RecompuiResource id, RecompuiFlexDirection flex_direction));
+RECOMP_IMPORT("*", void recompui_set_align_items(RecompuiResource id, RecompuiAlignItems align_items));
+
+// Overflow
+RECOMP_IMPORT("*", void recompui_set_overflow(RecompuiResource id, RecompuiOverflow overflow));
+RECOMP_IMPORT("*", void recompui_set_overflow_x(RecompuiResource id, RecompuiOverflow overflow));
+RECOMP_IMPORT("*", void recompui_set_overflow_y(RecompuiResource id, RecompuiOverflow overflow));
+
+// Text and Fonts
+RECOMP_IMPORT("*", void recompui_set_font_size(RecompuiResource id, float size, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_letter_spacing(RecompuiResource id, float spacing, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_line_height(RecompuiResource id, float height, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_font_style(RecompuiResource id, RecompuiFontStyle style));
+RECOMP_IMPORT("*", void recompui_set_font_weight(RecompuiResource id, unsigned long weight));
+RECOMP_IMPORT("*", void recompui_set_text_align(RecompuiResource id, RecompuiTextAlign text_align));
+
+// Gaps
+RECOMP_IMPORT("*", void recompui_set_gap(RecompuiResource id, float size, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_row_gap(RecompuiResource id, float size, RecompuiUnit unit));
+RECOMP_IMPORT("*", void recompui_set_column_gap(RecompuiResource id, float size, RecompuiUnit unit));
+
+// Drag and Focus
+RECOMP_IMPORT("*", void recompui_set_drag(RecompuiResource id, RecompuiDrag drag));
+RECOMP_IMPORT("*", void recompui_set_tab_index(RecompuiResource id, RecompuiTabIndex focus));
+
+#endif
--- src/always_spin_attack.c
+++ src/always_spin_attack.c
@@ -1,11 +1,83 @@
 #include "modding.h"
 #include "global.h"
+#include "recompui.h"
 
 RECOMP_IMPORT("*", int recomp_printf(const char* fmt, ...));
+RECOMP_IMPORT("*", u32 recomp_get_config_u32(const char* key));
+RECOMP_IMPORT("*", double recomp_get_config_double(const char* key));
+RECOMP_IMPORT("*", char* recomp_get_config_string(const char* key));
+RECOMP_IMPORT("*", void recomp_free_config_string(char* str));
+
+RecompuiContext context;
+RecompuiResource root;
+RecompuiResource outer_spacer1;
+RecompuiResource outer_spacer2;
+RecompuiResource inner_container;
+RecompuiResource inner_spacer1;
+RecompuiResource inner_spacer2;
+RecompuiResource button;
+
+bool context_shown = false;
+
+RecompuiResource create_spacer(RecompuiContext context, RecompuiResource parent, float flex_grow, float flex_shrink, float flex_basis) {
+    RecompuiResource ret = recompui_create_element(context, parent);
+
+    recompui_set_flex_basis(ret, flex_basis, UNIT_DP);
+    recompui_set_flex_grow(ret, flex_grow);
+    recompui_set_flex_shrink(ret, flex_shrink);
+
+    return ret;
+}
+
+RECOMP_CALLBACK("*", recomp_on_init)
+void on_init() {
+    context = recompui_create_context();
+
+    recompui_open_context(context);
+
+    root = recompui_context_root(context);
+
+    // Split the root into 3 columns.
+    outer_spacer1 = create_spacer(context, root, 1.0f, 0.0f, 0.0f);
+
+    inner_container = recompui_create_element(context, root);
+    recompui_set_display(inner_container, DISPLAY_FLEX);
+    recompui_set_flex_direction(inner_container, FLEX_DIRECTION_COLUMN);
+
+    outer_spacer2 = create_spacer(context, root, 1.0f, 0.0f, 0.0f);
+
+    // Split the middle column up into 3 rows.
+    inner_spacer1 = create_spacer(context, inner_container, 1.0f, 0.0f, 0.0f);
+
+    button = recompui_create_button(context, inner_container, "Mod button", BUTTONSTYLE_PRIMARY);
+    recompui_set_flex_grow(button, 0.0f);
+    recompui_set_flex_shrink(button, 0.0f);
+    recompui_set_flex_basis(button, 100.0f, UNIT_DP);
+    recompui_set_width(button, 200.0f, UNIT_DP);
+    
+    inner_spacer2 = create_spacer(context, inner_container, 1.0f, 0.0f, 0.0f);
+
+    recompui_close_context(context);
+}
 
 // Patches a function in the base game that's used to check if the player should quickspin.
 RECOMP_PATCH s32 Player_CanSpinAttack(Player* this) {
-    recomp_printf("Spin attacking\n");
+    recomp_printf("enum_option: %d\n", recomp_get_config_u32("enum_option"));
+    recomp_printf("number_option: %d\n", recomp_get_config_double("number_option"));
+    char* string_option = recomp_get_config_string("string_option");
+    if (string_option != NULL) {
+        recomp_printf("string_option: %s\n", string_option);
+        recomp_free_config_string(string_option);
+    }
+
+    if (context_shown) {
+        recompui_hide_context(context);
+    }
+    else {
+        recompui_show_context(context);
+    }
+    context_shown = !context_shown;
+
     // Always spin attack.
     return true;
 }
