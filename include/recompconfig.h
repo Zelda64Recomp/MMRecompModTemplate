b4ca19d085f07854f3de4f247bf81850869001fe
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2025-04-08 02:22:17 -0400

    Add recomp_change_save_file export and documentation in recompconfig.h

--- include/recompconfig.h
+++ include/recompconfig.h
@@ -3,10 +3,21 @@
 
 #include "modding.h"
 
+// Reads a config value of the specified type and name for the mod that called these functions.
+// These correspond to the config schema provided in the mod's manifest.
 RECOMP_IMPORT("*", unsigned long recomp_get_config_u32(const char* key));
 RECOMP_IMPORT("*", double recomp_get_config_double(const char* key));
 RECOMP_IMPORT("*", char* recomp_get_config_string(const char* key));
+
+// Frees a value returned by `recomp_get_config_string`. MUST be called to prevent a memory leak.
 RECOMP_IMPORT("*", void recomp_free_config_string(char* str));
+
+// Gets the version of the mod that called this function. Writes the mod's version numbers into the provided pointers.
 RECOMP_IMPORT("*", void recomp_get_mod_version(unsigned long* major, unsigned long* minor, unsigned long* patch));
+
+// Swaps to using a different file. The new save file will be located at `<mod id>/<filename>.bin` in the normal saves folder.
+// Don't include `.bin` in the provided filename.
+// Be careful calling this function during normal gameplay as the game won't be aware that any currently loaded save data is outdated.
+RECOMP_IMPORT("*", void recomp_change_save_file(const char* filename));
 
 #endif
