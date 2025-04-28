7fa46ff0313a020296de491fddfcbf4f8d48491f
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2025-04-28 00:22:44 -0400

    Add navigation exports to recompui.h

--- include/recompui.h
+++ include/recompui.h
@@ -130,6 +130,13 @@ typedef enum {
     SLIDERTYPE_INTEGER
 } RecompuiSliderType;
 
+typedef enum {
+    NAVDIRECTION_UP,
+    NAVDIRECTION_RIGHT,
+    NAVDIRECTION_DOWN,
+    NAVDIRECTION_LEFT,
+} RecompuiNavDirection;
+
 typedef struct {
     unsigned long type;
     float duration;
@@ -272,6 +279,11 @@ RECOMP_IMPORT("*", char* recompui_get_input_text(RecompuiResource id));
 RECOMP_IMPORT("*", void recompui_set_input_value_u32(RecompuiResource id, unsigned long val));
 RECOMP_IMPORT("*", void recompui_set_input_value_float(RecompuiResource id, float val));
 RECOMP_IMPORT("*", void recompui_set_input_text(RecompuiResource id, const char* text));
+
+// Navigation
+RECOMP_IMPORT("*", void recompui_set_nav_auto(RecompuiResource id, RecompuiNavDirection direction));
+RECOMP_IMPORT("*", void recompui_set_nav_none(RecompuiResource id, RecompuiNavDirection direction));
+RECOMP_IMPORT("*", void recompui_set_nav(RecompuiResource id, RecompuiNavDirection direction, RecompuiResource target_id));
 
 // Images
 RECOMP_IMPORT("*", RecompuiTextureHandle recompui_create_texture_rgba32(void* data, unsigned long width, unsigned long height));
